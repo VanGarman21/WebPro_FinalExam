2f98d04a1da721abba79b8880982ed186313fcc1
Van Garman <armand.giovani21@gmail.com>
2023-12-21 21:54:16 +0700

    feat: add ASP project

---
+++ FinalExamWebPro/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FinalExamWebPro.Data;
+
+namespace FinalExamWebPro.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<FinalExamWebPro.Data.Book> Book { get; set; }
+    }
+}
---
+++ FinalExamWebPro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalExamWebPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
